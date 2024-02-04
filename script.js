86fe73e0d5ba86c764d818f4b11566ecd908004f
Preet-savliya <preetsavliyaa@gmail.com>
2024-02-05 00:29:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var sidemenu = document.getElementById("sidemenu");
+
+        function openmenu() {
+            sidemenu.style.left = "0";
+        }
+
+        function colsemenu() {
+            sidemenu.style.left = "-100%";
+}
